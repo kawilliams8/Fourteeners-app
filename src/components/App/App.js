f91f322ee5be44206aa035aa89c845911aeda47e
Katie Williams <kawilliams8@gmail.com>
2020-12-06 20:46:38 -0700

    Add functional component to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "App.css";
-import App from "./App";
+
+function App() {
+  return (
+    <div>
+      App Component
+    </div>
+  );
+}
+
+export default App;
