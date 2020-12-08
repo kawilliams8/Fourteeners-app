509339d5a40ee021a2703fd659a7e80cfba01b78
Katie Williams <kawilliams8@gmail.com>
2020-12-07 18:12:29 -0700

    Update webpack start scripts, first app appearance on the DOM

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import ".index.css";
-import App from "./src/components/App";
+import React from 'react';
+import {render} from 'react-dom';
+import './index.css';
+import App from './src/components/App/App.js';
+
+render(<App/>,
+document.getElementById('root')
+)
