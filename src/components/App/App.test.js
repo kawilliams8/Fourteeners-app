6250b8e0393ed2664664d4b9fe9aea7b2f160582
Katie Williams <kawilliams8@gmail.com>
2020-12-07 19:22:09 -0700

    Add initial App test, with enzyme snapshots set up

---
+++ src/components/App/App.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { shallow } from "enzyme";
+
+describe("App", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it("should match the snapshot", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
