ce23df0ecaf98ccccc41d1286ed27699f8fd0f5e
Katie Williams <kawilliams8@gmail.com>
2020-12-07 19:55:15 -0700

    Install react-router and set up in index.js

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import {render} from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './src/components/App/App.js';
 
-render(<App/>,
+const router = (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
+render(router,
 document.getElementById('root')
 )
