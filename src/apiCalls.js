1f92f0004276b025eb1a1e4d992490a84f559ea7
Katie Williams <kawilliams8@gmail.com>
2020-12-08 21:43:18 -0700

    Add getPeak apiCall and tests

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -11,6 +11,19 @@ export const getPeaks = () => {
     });
 };
 
+export const getPeak = (id) => {
+  return fetch(`https://fourteeners-api.herokuapp.com/api/v1/peaks/${id}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw Error("Fetching Error");
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw Error(error.message);
+    });
+};
+
 export const deletePeak = (id) => {
   return fetch(`https://fourteeners-api.herokuapp.com/api/v1/peaks/${id}`)
     .then((response) => {
--- src/apiCalls.test.js
+++ src/apiCalls.test.js
@@ -1,6 +1,6 @@
-import { getPeaks, deletePeak } from "./apiCalls";
+import { getPeaks, getPeak, deletePeak } from "./apiCalls";
 
-describe("getPeaks", () => {
+describe("getPeaks - GET an array of all peaks", () => {
   let mockPeak;
   beforeEach(() => {
     mockPeak = [
@@ -69,6 +69,91 @@ describe("getPeaks", () => {
     });
 
     expect(getPeaks()).rejects.toEqual(Error("Fetching Error"));
+  });
+
+  it("should show an error when the fetch Promise returns rejected -- message: 'Fetching Error'", () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        message: "Fetching Error",
+      });
+    });
+
+    expect(getPeaks()).rejects.toEqual(Error("Fetching Error"));
+  });
+});
+
+describe("getPeak - GET one matching peak object, by id", () => {
+  let mockPeak;
+  beforeEach(() => {
+    mockPeak = [
+      {
+        id: 1,
+        name: "Mount Elbert",
+        elevation: 14433,
+        rank: 1,
+        range: "Sawatch",
+        forest: "San Isabel",
+        grizzlyBears: false,
+        marmots: true,
+        jerryLevel: "high",
+        numberOfRoutes: 4,
+        routes: {
+          northeastRidge: {
+            mileage: 9.5,
+            gain: 4700,
+            difficulty: "class 1",
+            exposure: 1,
+          },
+          eastRidge: {
+            mileage: 10,
+            gain: 4100,
+            difficulty: "class 1",
+            exposure: 1,
+          },
+          southeastRidge: {
+            mileage: 11,
+            gain: 5300,
+            difficulty: "class 2",
+            exposure: 1,
+          },
+          boxCreekCouloir: {
+            mileage: 8.5,
+            gain: 4150,
+            difficulty: "class 2+",
+            exposure: 3,
+          },
+        },
+      },
+    ];
+
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(mockPeak),
+      });
+    });
+  });
+
+  it("should call fetch with the correct URL", () => {
+    getPeak(1);
+    expect(window.fetch).toHaveBeenCalledWith(
+      "https://fourteeners-api.herokuapp.com/api/v1/peaks/1"
+    );
+  });
+
+  it("should return one matching peak object", () => {
+    expect(getPeak(1)).resolves.toEqual(mockPeak);
+    expect(getPeak(1).name).toEqual(mockPeak.name);
+  });
+
+  it("should show an error when the fetch Promise returns rejected -- ok: false", () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: false,
+      });
+    });
+
+    expect(getPeak(1)).rejects.toEqual(Error("Fetching Error"));
   });
 
   it("should show an error when the fetch Promise returns rejected -- message: 'Fetching Error'", () => {
